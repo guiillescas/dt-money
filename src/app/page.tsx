f1681ead69cf0b5b279536f33607b122d5292082
Guilherme Illescas <gui.illescas@icloud.com>
2023-04-13 20:12:46 -0300

    fix: removde prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import { Roboto } from 'next/font/google'
 const inter = Roboto({ weight: '400', subsets: ['latin'] })
 
 //<h2 className={inter.className}>
-// 25:32
 export default function Home() {
   return <h1>a</h1>
 }
